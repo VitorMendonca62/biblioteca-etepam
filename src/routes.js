cc9717ecf5dd64e3ac7ea016cebfe79ce3ca80ca
VitorMendonca62 <vitorqueiroz325@gmail.com>
2022-06-11 17:12:40 -0300

    criação de rotas para arquivos estaticos

--- src/routes.js
+++ src/routes.js
@@ -14,22 +14,22 @@ routes.get("/auth/cadastrar-user", UserController.index);
 
 routes.post("/books/cadaster-book", BookController.store)
 routes.get("/books/cadaster-book", BookController.index);
+routes.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../public/html/login.html"));
+});
+routes.get("/cadastro", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../public/html/cadastro.html"));
+});
+routes.get("/matricula", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname + "/../public/assets/Como conseguir a sua matricula.pdf"
+    )
+  );
+});
+routes.get("/cadastro-livro", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../public/html/cadastro-livro.html"));
+});
 
 module.exports = routes;
 
-// routes.get("/login", (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "/../public/html/login.html"));
-// });
-// routes.get("/cadastro", (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "/../public/html/cadastro.html"));
-// });
-// routes.get("/matricula", (req, res) => {
-//   res.sendFile(
-//     path.resolve(
-//       __dirname + "/../public/assets/Como conseguir a sua matricula.pdf"
-//     )
-//   );
-// });
-// routes.get("/cadastro-livro", (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "/../public/html/cadastro-livro.html"));
-// });
